c90aa2846067a644513abfd0ddb7febe994554c8
3v0k4 <riccardo.odone@gmail.com>
2014-08-23 12:45:39 +0200

    added firstname and lastname to User

---
+++ db/migrate/20140823103420_add_firstname_and_lastname_to_users.rb
@@ -0,0 +1,6 @@
+class AddFirstnameAndLastnameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :firstname, :string, null: false
+    add_column :users, :lastname, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823102744) do
+ActiveRecord::Schema.define(version: 20140823103420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20140823102744) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "firstname",                           null: false
+    t.string   "lastname",                            null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
