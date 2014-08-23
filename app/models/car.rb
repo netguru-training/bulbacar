6f2776b4dd82a4653e6348e13f6c0fb9e2b6ec59
Krzysztof Rój <git@krzysztofroj.pl>
2014-08-23 12:18:58 +0200

    Fix typo - spaces!

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ActiveRecord::Base
   belongs_to :user
-  AVAILABLE_SEATS = [ 1, 2, 3, 4, 5, 6 ]
+  AVAILABLE_SEATS = [1, 2, 3, 4, 5, 6]
 
   validates :brand, presence: true
   validates :model, presence: true
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :reviewer, class_name: "User"
   belongs_to :reviewed, class_name: "User"
-  RATING = [ 1, 2, 3, 4, 5 ]
+  RATING = [1, 2, 3, 4, 5]
 
   validates :rating, presence: true, inclusion: { in: RATING }
   validates :comment, presence: true
