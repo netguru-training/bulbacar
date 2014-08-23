90a9f2ab5af06277a91b33f6fae6f8d26c1ba536
Krzysztof Rój <git@krzysztofroj.pl>
2014-08-23 12:27:24 +0200

    Refactor code to follow conventions

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,8 @@
 class Car < ActiveRecord::Base
-  belongs_to :user
   AVAILABLE_SEATS = [1, 2, 3, 4, 5, 6]
 
+  belongs_to :user
+  
   validates :brand, presence: true
   validates :model, presence: true
   validates :seats, presence: true, inclusion: { in: AVAILABLE_SEATS }
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ActiveRecord::Base
-  belongs_to :reviewer, class_name: "User"
-  belongs_to :reviewed, class_name: "User"
   RATING = [1, 2, 3, 4, 5]
 
+  belongs_to :reviewer, class_name: "User"
+  belongs_to :reviewed, class_name: "User"
+  
   validates :rating, presence: true, inclusion: { in: RATING }
   validates :comment, presence: true
   validates :reviewer_id, presence: true
