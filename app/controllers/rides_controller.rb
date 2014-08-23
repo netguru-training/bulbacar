b810f9567381fb6cbeac842338c65a19f4bae553
3v0k4 <riccardo.odone@gmail.com>
2014-08-23 17:24:03 +0200

    basic structure to create new ride

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,8 +2,12 @@ class RidesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   expose(:rides) 
+  expose(:ride)
 
   def index
+  end
+  
+  def new
   end
 
 end
