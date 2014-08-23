0a1d4651e3d593142ac711e8e67db81397ce0144
Rafal Radziszewski <rafal.radziszewski@icloud.com>
2014-08-23 12:15:30 +0200

    Point and Route model fixes

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,2 +1,9 @@
 class Point < ActiveRecord::Base
+  belongs_to :route
+
+  validates :name, presence: true
+  validates :number, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,8 @@
 class Route < ActiveRecord::Base
+  belongs_to :user
+  has_many :points
+
+  validates :date, presence: true
+  validates :description, presence: true
+
 end
