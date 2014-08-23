28674ce299f29dcc9f1b3d173102c58ed2b61d50
Krzysztof Rój <git@krzysztofroj.pl>
2014-08-23 12:09:55 +0200

    Refactor Car model with spaces!

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,9 @@
 class Car < ActiveRecord::Base
   belongs_to :user
+  AVAILABLE_SEATS = [ 1, 2, 3, 4, 5, 6 ]
 
   validates :brand, presence: true
   validates :model, presence: true
-  validates :seats, presence: true, numericality: { greater_than: 0 }
+  validates :seats, presence: true, inclusion: { in: AVAILABLE_SEATS }
 
 end
