52d45a576e413ead7d0ee76320b94b8bb4213cbb
Rafal Radziszewski <rafal.radziszewski@icloud.com>
2014-08-23 16:06:42 +0200

    Seeds and car model fixed

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,8 @@
 class Car < ActiveRecord::Base
   AVAILABLE_SEATS = [1, 2, 3, 4, 5, 6]
 
-  belongs_to :user
-  
+  belongs_to :owner, class_name: 'User'
+
   validates :brand, presence: true
   validates :model, presence: true
   validates :seats, presence: true, inclusion: { in: AVAILABLE_SEATS }
