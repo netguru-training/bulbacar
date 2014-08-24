4ba92e5dbbbacdd3d37b0b3dd28c4f0f847b9fdb
3v0k4 <riccardo.odone@gmail.com>
2014-08-24 15:43:49 +0200

    seeds tweaked

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,11 +28,11 @@ Car.create!(owner_id: u4.id, brand: "Toyota", model: "Prius", seats: 4)
 
 time = Time.now + (60 * 60 * 8)
 
-r1 = Ride.create!(departure_at: time, owner: u2, description: "Jade na wakacje!")
+r1 = Ride.create!(departure_at: time, owner: u2, description: "Jade na wakacje!", finished: true)
 r2 = Ride.create!(departure_at: time + 60, owner: u2, description: "Jade do babci!")
 r3 = Ride.create!(departure_at: time + 120, owner: u3, description: "Jade bo mi się nudzi")
 r4 = Ride.create!(departure_at: time + 180, owner: u3, description: "Jade!")
-r5 = Ride.create!(departure_at: time + 240, owner: u1, description: "Jade bo tak!")
+r5 = Ride.create!(departure_at: time + 240, owner: u1, description: "Jade bo tak!", finished: true)
 
 Point.create!(name: "Poznan", number: 0, ride: r1, latitude: 52.458123, longitude: 16.921312)
 Point.create!(name: "Warszawa", number: 1, ride: r1, latitude: 12.458123, longitude: 22.921312)
@@ -46,4 +46,5 @@ Point.create!(name: "Krakow", number: 1, ride: r4, latitude: 62.458123, longitud
 Point.create!(name: "Gdansk", number: 0, ride: r5, latitude: 32.458123, longitude: 17.921312)
 Point.create!(name: "Krakow", number: 1, ride: r5, latitude: 62.458123, longitude: 21.921312)
 
-UserRide.create!(user_id: u5, ride_id: r1, status: "pending")
+UserRide.create!(user_id: u5.id, ride_id: r1.id, status: "pending")
+accepted_ride = UserRide.create!(user_id: u1.id, ride_id: r1.id, status: "accepted")
