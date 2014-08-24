27e4ca761307f5a080cfae7f877a705606a43d45
3v0k4 <riccardo.odone@gmail.com>
2014-08-24 15:41:27 +0200

    review index and create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,26 @@
 class ReviewsController < ApplicationController
   expose(:attended_rides) { current_user.attended_rides }
+  expose(:review)
 
   def index
   end
+  
+  def new
+  end
+  
+  def create
+    self.review = Review.new(review_params)
+    binding.pry
+    if self.review.save
+      redirect_to reviews_path
+    else
+      render action: 'new'
+    end
+  end
 
+  def review_params
+    params.require(:review).permit(:reviewer_id, :ride_id, :rating, :comment)
+  end
+  
+  
 end
