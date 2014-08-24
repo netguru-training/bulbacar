676114fac161f4d382faacc51f5c3fbf661f4933
3v0k4 <riccardo.odone@gmail.com>
2014-08-24 12:10:43 +0200

    added assets for nested_form and datetime picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require moment
+//= require bootstrap-datetimepicker
+//= require pickers
+//= require cocoon
 //= require_tree .
