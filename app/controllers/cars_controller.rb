53b436d9be67700f938eaa7466554bf03ba31a1b
Rafal Radziszewski <rafal.radziszewski@icloud.com>
2014-08-24 10:54:20 +0200

    Car controler and view done(validation messages to fix - if there is time

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,23 +5,28 @@ class CarsController < ApplicationController
 
 
 
-  # def create
-  #   self.car = Car.new(car_params)
-  #
-  #   if car.save
-  #     redirect_to user_cars_url(user)
-  #   else
-  #     render action: 'new'
-  #   end
-  # end
-  #
-  # def update
-  #   if self.car.update(car_params)
-  #     redirect_to user_cars_url(user)
-  #   else
-  #     render action: 'edit'
-  #   end
-  # end
+  def create
+    self.car = Car.new(car_params)
+
+    if self.car.save
+      current_user.cars << car
+      flash[:success] = 'Car successfully added!'
+      redirect_to user_cars_path(current_user)
+    else
+      render action: 'new'
+      flash[:error]: = "Couldn't create the car!"
+    end
+  end
+
+  def update
+    if self.car.update(car_params)
+      flash[:success] = 'Car successfully updated!'
+      redirect_to user_cars_path(current_user)
+    else
+      render action: 'edit'
+      flahs[:error] = "Couldn't update the car!"
+    end
+  end
 
   def new
   end
@@ -29,13 +34,20 @@ class CarsController < ApplicationController
   def index
   end
 
-  # def destroy
-  #   if self.car.destroy
-  #
-  # private
-  #
-  # def car_params
-  #   params.require(:car).permit(:brand, :model, :seats)
-  # end
+  def destroy
+    if self.car.destroy
+      flash[:success] = 'Car deleted!'
+      redirect_to user_cars_path(current_user)
+    else
+      render action: 'index'
+      flash[:error] = "Couldn't destroy the car!"
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :seats)
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def bootstrap_class_for(flash_type)
       {
-        :success => 'alert-success',
-        :error => 'alert-danger',
-        :alert => 'alert-warning',
-        :notice => 'alert-info'
+        success: 'alert alert-success',
+        error: 'alert alert-danger',
+        alert: 'alert alert-warning',
+        notice: 'alert alert-info'
       }[flash_type.to_sym] || flash_type.to_s
     end
 end
